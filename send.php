566909c8bebbe3f6b9aaecbbee9e8f3245835cd7
ganeshpd188 <37778502+ganeshpd188@users.noreply.github.com>
2018-12-06 01:07:15 +0530

    Update send.php

--- send.php
+++ send.php
@@ -5,7 +5,7 @@ $subject=$_POST['subject'];
 $message=$_POST['message'];
 $body="You have been successfully registered with us!";
 $from_new="From: $from";
-mail($to,$subject,$body,$from_new);
+mail($to,$subject,$message,$from_new);
 echo "Email has been sent to the mentioned email addess!";
 
 ?>
